c201bddef0a4ed62180d03a2c5c68aeeebf08af6
cmarshall <cmarshall@smith.edu>
2019-07-29 11:12:12 -0400

    Ran on prod; created people agents

--- mrbc_resources/create_mrbc_agents.py
+++ mrbc_resources/create_mrbc_agents.py
@@ -17,15 +17,17 @@ def create_new_people_agent(name):
 			'agent_type': 'agent_person'}
 
 	name_dict = {}
-	name = name.split(',')
-	if len(re.findall(r'\d+', name[-1])) != 0:
-		name_dict['dates'] = name[-1].strip()
+	namex = name.split(',')
+	if re.findall('--', name):
+		name_dict['primary_name'] = name
+	if len(re.findall(r'\d+', namex[-1])) != 0:
+		name_dict['dates'] = namex[-1].strip()
 	try:
-		name_dict['primary_name'] = name[0].strip()
+		name_dict['primary_name'] = namex[0].strip()
 	except:
 		pass
 	try:
-		name_dict['rest_of_name'] = name[1].strip()
+		name_dict['rest_of_name'] = namex[1].strip()
 	except:
 		pass
 	name_dict['is_display_name'] = True
@@ -54,35 +56,61 @@ if __name__ == "__main__":
 	logging.basicConfig(level=logging.INFO)
 
 
-	csv = pd.read_csv('mrbc_agents.csv')
-	agents = csv.creator_person # Column name
+	# csv = pd.read_csv('crosschecked_resources.csv')
+	
+	''' Code to create MRBC JSON file '''
+	# csv_file = pd.DataFrame(pd.read_csv('crosschecked_resources.csv', sep = ",", header = 0, index_col = False))
+	# csv_file.to_json("mrbc_to_aspace.json", orient = "records", date_format = "epoch", double_precision = 10, force_ascii = True, date_unit = "ms", default_handler = None)
 
-	names = []
-	for agent in agents:
-		name = unidecode.unidecode(agent)
-		names.append(name)
+	with open("mrbc_to_aspace.json") as json_file:
+		try:
+			resources = json.load(json_file)
+		except ValueError:
+			logging.info('File not found')
+			exit(1)
 
+	# Extracting the keys that contain person agent data to be created
+	person_keys = []
+	for resource in resources:
+		for key in resource.keys():
+			if re.findall('person', key):
+				if key not in person_keys:
+					person_keys.append(key)
+
+	# Pulling out the name values from the keys
 	count = 0
-	new_uris = []
-	for name in names:
-		if len(name) != 0:
-			count += 1
-			new_agent = create_new_people_agent(name)
-			try:
-				new_agent_created = aspace.post('agents/people', new_agent)
-				print(count)
-				new_uris.append(new_agent_created['uri'])
-			except:
-				pprint.pprint(new_agent)
+	new_agent_names = {}
+	new_agent_names['data'] = []
+	errors = {}
+	errors['data'] = []
+	for resource in resources:
+		for key in person_keys:
+			if resource[key] != None and not 'agents' in resource[key]:
+				name = unidecode.unidecode(resource[key])
+				new_agent = create_new_people_agent(name)
+				try:
+					count += 1
+					print(count)
+					new_agent_created = aspace.post('agents/people', new_agent)
+					pprint.pprint(new_agent_created)
+					new_agent_names['data'].append((name, new_agent_created['uri']))
+				except:
+					logging.info('Agent failed to be created')
+					errors['data'].append(new_agent)
 
 
-	csv_input = pd.read_csv('mrbc_agents.csv')
-	csv_input['agent_uri'] = new_uris
-	print(csv_input)
-	csv_input.to_csv('pandas_test.csv', index=False)
+	with open('new_agent_names.txt', 'w') as outfile:
+		json.dump(new_agent_names, outfile)
 
-	# test = aspace.get('/agents/people/45863')
-	# pprint.pprint(test)
+	with open('agents_not_created.txt', 'w') as outfile:
+		json.dump(errors, outfile)
+
+
+
+
+
+
+
 
 
 
